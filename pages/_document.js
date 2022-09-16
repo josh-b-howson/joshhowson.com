6f6a8f8fe726d04cfc14f8fd3f27eebb0122efd3
josh-b-howson <josh.b.howson@gmail.com>
2022-09-15 21:12:02 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -8,8 +9,18 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;900&display=swap" rel="stylesheet" />
         <meta name="color-scheme" content="dark light"></meta>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6GFT8FSH33"></script>
       </Head>
       <body>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-6GFT8FSH33');
+            `}} />
         <Main />
         <NextScript />
       </body>
