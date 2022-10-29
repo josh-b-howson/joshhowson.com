d9f6e7719ccc525303e5bf76af652c965fd4c30c
josh-b-howson <josh.b.howson@gmail.com>
2022-10-28 21:39:20 -0700

    Let's just not use google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,17 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;900&display=swap" rel="stylesheet" />
-        <meta name="color-scheme" content="dark light"></meta>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6GFT8FSH33"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-6GFT8FSH33');
-            `}} />
+        <meta name="color-scheme" content="dark light" />
       </Head>
       <body>
         <Main />
