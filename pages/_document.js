d9bbab197062c0ff43299a4d3916081c00eb1af4
josh-b-howson <josh.b.howson@gmail.com>
2022-10-28 21:40:56 -0700

    test no custom fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;900&display=swap" rel="stylesheet" /> */}
         <meta name="color-scheme" content="dark light" />
       </Head>
       <body>
